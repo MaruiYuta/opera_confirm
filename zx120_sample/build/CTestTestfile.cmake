d534a89c884d70aa0fcae1ed33ad26dcf03e70f5
MaruiYuta <s2320648@u.tsukuba.ac.jp>
2024-05-20 14:35:14 +0900

    フォルダーを削除

--- zx120_sample/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marui-y/zx120_sample/src
-# Build directory: /home/marui-y/zx120_sample/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("internship_task")
